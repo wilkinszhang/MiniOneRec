f5fa2376dea1a0af294f6403ac53ceaaffb5602d
Xiaoyu Kong <44406991+AkaliKong@users.noreply.github.com>
2025-10-29 14:35:50 +0800

    Update main.sh

--- rq/main.sh
+++ rq/main.sh
@@ -4,6 +4,3 @@ python main.py \
       --lr 1e-3 \
       --epochs 10000 \
       --batch_size 20480
-
-# /home/huangyanwen.hyw/code_linlin/LC-Rec-main/data/Industrial_and_Scientific/Industrial_and_Scientific.emb-qwen-td.npy
-# /home/huangyanwen.hyw/code_linlin/LC-Rec-main/data/Sports/Sports.emb-qwen-td.npy

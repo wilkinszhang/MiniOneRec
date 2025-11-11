8c5aada81f4a45009499aff348df8716eb380762
Xiaoyu Kong <44406991+AkaliKong@users.noreply.github.com>
2025-11-11 20:56:49 +0800

    Update rqvae.sh

--- rq/rqvae.sh
+++ rq/rqvae.sh
@@ -1,6 +1,6 @@
-python main.py \
-      --data_path xxx/data/Office_Products/Office_Products.emb-qwen-td.npy \
-      --ckpt_dir ./output/Office_Products \
+python rqvae.py \
+      --data_path ../data/Amazon/index/Industrial_and_Scientific.emb-qwen-td.npy \
+      --ckpt_dir ./output/Industrial_and_Scientific \
       --lr 1e-3 \
       --epochs 10000 \
       --batch_size 20480
